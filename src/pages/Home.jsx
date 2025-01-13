d6f347d6342de97fd3f050f2b85c273638b61940
lakshmi2911 <lakshmipriya1129@gmail.com>
2025-01-13 12:25:55 +0530

    Cart Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,76 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchProducts } from '../redux/slices/productSlice'
 
 const Home = () => {
   const dispatch =useDispatch()
+  const {allProducts,loading,errorMsg} = useSelector(state=>state.productReducer)
+  // console.log(allProducts,loading,errorMsg);
+  const [currentPage,setCurrentPage] = useState(1)
+  const productPerPage = 8
+  const totalPages = Math.ceil(allProducts?.length/productPerPage)
+  const currentPageProductLastIndex = currentPage * productPerPage
+  const currentPageProductFirstIndex =  currentPageProductLastIndex-productPerPage
+  const visibleAllProducts = allProducts?.slice(currentPageProductFirstIndex,currentPageProductLastIndex)
+  
 
   useEffect(()=>{
     dispatch(fetchProducts())
   },[])
+
+  const navigateToNextPage =()=>{
+    if(currentPage!=totalPages){
+      setCurrentPage(currentPage+1)
+    }
+  }
+
+  const navigateToPrevPage =()=>{
+    if(currentPage!=1){
+      setCurrentPage(currentPage-1)
+    }
+  }
 
 
   return (
     <>
     <Header insideHome={true}/>
     <div style={{paddingTop:'100px'}} className='container px-4 mx-auto'>
-      <div className='grid grid-cols-4 gap-4'>
-        <div className='rounded border p-2 shadow'>
-          <img width={'100%'} height={'200px'} src="https://img.freepik.com/free-vector/shopping-supermarket-cart-with-grocery-pictogram_1284-11697.jpg?semt=ais_hybrid" alt="" />
-          <div className='text-center'>
-            <h3 className='text-xl font -bold'>Product name</h3>
-            <Link to={'/id/view'} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block '>View More..</Link>
-          </div>
+      {
+        loading ?
+        <div className='flex justify-center items-center my-5 text-lg'>
+          <img width={'80px'} height={'80px'} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKRrAQ4k9MEwAZpr_LmlY6jvkV1IUk7gIq7Ls3lKVxD3so8O2taw9buQQgQRD1HkDAOak&usqp=CAU" alt="" />
+          Loading....
         </div>
-      </div>
+        :
+        <>
+          <div className='grid grid-cols-4 gap-4'>
+            {
+              allProducts?.length>0 ?
+              visibleAllProducts?.map(product=>(
+                <div key={product?.id} className='rounded border p-2 shadow'>
+                  <img width={'100%'} height={'100%'} src= {product?.thumbnail} alt="" />
+                  <div className='text-center'>
+                    <h3 className='text-xl font -bold'>{product?.title}</h3>
+                    <Link to={`/${product?.id}/view`} className='bg-violet-600 rounded p-1 mt-3 text-white inline-block '>View More..</Link>
+                  </div>
+                </div>
+                ))
+                :
+                <div className='flex justify-center items-center font-bold text-red-600 my-5 text-lg'>
+                  Product Not found!!!
+                </div>
+            }
+          </div>
+          <div className='text-2xl text-center font-bold mt-20'>
+            <span onClick={navigateToPrevPage} className='cursor-pointer'><i className='fa-solid fa-backward me-5'></i></span>
+            <span >{currentPage} of {totalPages}</span>
+            <span onClick={navigateToNextPage} className='cursor-pointer'><i className='fa-solid fa-forward ms-5'></i></span>
+          </div>
+          
+        </>
+      }
     </div>
       
     </>
--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -4,7 +4,8 @@ import axios from "axios";
 // action return promise
 export const fetchProducts = createAsyncThunk("products/fetchProducts", async ()=>{
     const result = await axios.get("https://dummyjson.com/products")
-    console.log(result);   
+    // console.log(result);
+    sessionStorage.setItem("allproducts",JSON.stringify(result.data.products))  
     return result.data.products 
 })
 
@@ -12,28 +13,36 @@ const productSlice = createSlice({
     name:"products",
     initialState:{
         allProducts:[],
+        dummyAllProducts :[],
         loading:false,
         errorMsg:""
     },
     reducers:{
+        searchProduct: (state,actionByHeader)=>{
+            state.allProducts = state.dummyAllProducts.filter(item=>item.title.toLowerCase().includes(actionByHeader.payload))
+        }
 
     },
     extraReducers:(builder)=>{
         builder.addCase(fetchProducts.fulfilled,(state,apiResult)=>{
             state.allProducts = apiResult.payload
+            state.dummyAllProducts = apiResult.payload
             state.loading = false
             state.errorMsg = ""
         })
         builder.addCase(fetchProducts.pending,(state,apiResult)=>{
             state.allProducts = []
+            state.dummyAllProducts = []
             state.loading = true
             state.errorMsg = ""
         })
         builder.addCase(fetchProducts.rejected,(state,apiResult)=>{
             state.allProducts = []
+            state.dummyAllProducts = []
             state.loading = false
             state.errorMsg = "API call failed"
         })
     }
 })
+export const {searchProduct} = productSlice.actions
 export default productSlice.reducer
